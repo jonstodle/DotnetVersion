18008ed47a100558019768c9596b1b933f68664b
Jon Grythe Stødle <jon@indicosys.com>
2019-06-13 15:21:41 +0200

    Don't choke on pre-release and meta information after patch version number

--- src/DotnetVersion/Program.cs
+++ src/DotnetVersion/Program.cs
@@ -72,34 +72,44 @@ namespace DotnetVersion
             var versionElement = xDocument.Root?.Descendants("Version").FirstOrDefault();
             var currentVersion = versionElement?.Value ?? "0.0.0";
 
-            var (major, minor, patch) = ParseVersion(currentVersion);
+            var (major, minor, patch, meta) = ParseVersion(currentVersion);
 
             if (!string.IsNullOrWhiteSpace(NewVersion))
             {
-                (major, minor, patch) = ParseVersion(NewVersion);
+                (major, minor, patch, meta) = ParseVersion(NewVersion);
             }
             else if (Major)
             {
                 major++;
                 minor = 0;
                 patch = 0;
+                meta = "";
             }
             else if (Minor)
             {
                 minor++;
                 patch = 0;
+                meta = "";
             }
             else if (Patch)
             {
-                patch++;
+                if (string.IsNullOrWhiteSpace(meta))
+                    patch++;
+                meta = "";
             }
             else
             {
                 var version = Prompt.GetString("Version number:");
-                (major, minor, patch) = ParseVersion(version);
+                (major, minor, patch, meta) = ParseVersion(version);
             }
 
-            var newVersion = string.Join(".", major, minor, patch);
+            var newVersion = string.Join(
+                ".",
+                major,
+                minor,
+                !string.IsNullOrWhiteSpace(meta)
+                    ? $"{patch}-{meta}"
+                    : patch.ToString());
             
             WriteLine($"Current version: {currentVersion}");
             WriteLine($"New version: {newVersion}");
@@ -148,7 +158,7 @@ namespace DotnetVersion
             WriteLine($"Successfully set version to {newVersion}");
         }
 
-        private (int major, int minor, int patch) ParseVersion(string version)
+        private (int major, int minor, int patch, string meta) ParseVersion(string version)
         {
             if (string.IsNullOrWhiteSpace(version))
                 throw new CliException(1, "Version string can not be empty.");
@@ -160,6 +170,7 @@ namespace DotnetVersion
             var major = 0;
             var minor = 0;
             var patch = 0;
+            var meta = "";
 
             if (versionValues.Length > 0)
                 if (!int.TryParse(versionValues[0], out major))
@@ -168,10 +179,16 @@ namespace DotnetVersion
                 if (!int.TryParse(versionValues[1], out minor))
                     throw new CliException(1, "Minor version number could not be parsed.");
             if (versionValues.Length > 2)
-                if (!int.TryParse(versionValues[2], out patch))
+            {
+                var patchValues = versionValues[2].Split('-');
+                
+                if (!int.TryParse(patchValues[0], out patch))
                     throw new CliException(1, "Patch version number could not be parsed.");
+                if (patchValues.Length > 1)
+                    meta = patchValues[1];
+            }
 
-            return (major, minor, patch);
+            return (major, minor, patch, meta);
         }
     }
 }
